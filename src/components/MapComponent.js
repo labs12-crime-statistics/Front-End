b8d9c44164887112b2acf9f84b992fc35de9ff6e
Michael Beck <brit228@gmail.com>
2019-04-25 18:52:37 -0400

    Added components for later

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,3 +1,5 @@
+// Google map component with overlay
+
 import React, {Component} from 'react';
 import {Map, Polygon, GoogleApiWrapper} from 'google-maps-react';
 import axios from 'axios';
@@ -12,19 +14,24 @@ class MapComponent extends Component {
     }
   }
 
-
   componentDidMount(){
-    console.log(process.env);
     new Promise((resolve) => axios.get("https://crimespot-backend.herokuapp.com/city/1/shapes")
       .then(function (response) {
         const colorMap = (alpha) => {
-          var alpha = Math.round(255 * alpha);
-          var beta = 255 - alpha;
-          alpha = alpha.toString(16).toUpperCase();
-          if (alpha.length < 2) {alpha = "0" + alpha}
-          beta = beta.toString(16).toUpperCase();
-          if (beta.length < 2) {beta = "0" + beta}
-          return("#"+beta+"00"+alpha);
+          var r = 0;
+          var g = 0;
+          if (alpha < 0.5) {
+            r = Math.round(255 * 2.0 *alpha);
+            g = 255;
+          } else {
+            r = 255;
+            g = Math.round(255 * 2.0 * (1.0 - alpha));
+          }
+          r = r.toString(16).toUpperCase();
+          if (r.length < 2) {r = "0" + r}
+          g = g.toString(16).toUpperCase();
+          if (g.length < 2) {g = "0" + g}
+          return("#"+r+g+"00");
         }
         var shapes = [];
         var shapeids = {};
@@ -43,11 +50,11 @@ class MapComponent extends Component {
             paths={paths}
             strokeColor="#FFFFFF"
             strokeOpacity={0.8}
-            strokeWeight={1}
+            strokeWeight={2}
             fillColor={color}
-            fillOpacity={0.2}
-            onMouseover={(p,o,e) => o.setOptions({fillOpacity: 0.15})}
-            onMouseout={(p,o,e) => o.setOptions({fillOpacity: 0.2})}
+            fillOpacity={0.3}
+            onMouseover={(p,o,e) => o.setOptions({fillOpacity: 0.18, strokeWeight: 6})}
+            onMouseout={(p,o,e) => o.setOptions({fillOpacity: 0.3, strokeWeight: 2})}
           />);
         });
         resolve({shapes, shapeids, coords});
@@ -71,338 +78,42 @@ class MapComponent extends Component {
   }
 }
 
+// Google API key
 export default GoogleApiWrapper({
   apiKey: "AIzaSyAiSsYgrWa71hZoeEEKaZZ2SB4nDOJxLsI"
 })(MapComponent);
 
+// Google map style
 const mapStyle = [
   {
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#212121'
-      }
-    ]
+      "stylers": [
+          {
+              "hue": "#2c3e50"
+          },
+          {
+              "saturation": 250
+          }
+      ]
   },
   {
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'off'
-      }
-    ]
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+          {
+              "lightness": 50
+          },
+          {
+              "visibility": "simplified"
+          }
+      ]
   },
   {
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#757575'
-      }
-    ]
-  },
-  {
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        color: '#212121'
-      }
-    ]
-  },
-  {
-    featureType: 'administrative',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#757575'
-      }
-    ]
-  },
-  {
-    featureType: 'administrative.country',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#9e9e9e'
-      }
-    ]
-  },
-  {
-    featureType: 'administrative.land_parcel',
-    stylers: [
-      {
-        visibility: 'off'
-      }
-    ]
-  },
-  {
-    featureType: 'administrative.locality',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#bdbdbd'
-      }
-    ]
-  },
-  {
-    featureType: 'poi',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#757575'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.attraction',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.attraction',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.business',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.business',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.government',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.government',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.medical',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.medical',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#181818'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#616161'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.text.stroke',
-    stylers: [
-      {
-        color: '#1b1b1b'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.place_of_worship',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.place_of_worship',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.school',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.school',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.sports_complex',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'poi.sports_complex',
-    elementType: 'labels.icon',
-    stylers: [
-      {
-        visibility: 'on'
-      }
-    ]
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry.fill',
-    stylers: [
-      {
-        color: '#2c2c2c'
-      }
-    ]
-  },
-  {
-    featureType: 'road',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#8a8a8a'
-      }
-    ]
-  },
-  {
-    featureType: 'road.arterial',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#373737'
-      }
-    ]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#3c3c3c'
-      }
-    ]
-  },
-  {
-    featureType: 'road.highway.controlled_access',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#4e4e4e'
-      }
-    ]
-  },
-  {
-    featureType: 'road.local',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#616161'
-      }
-    ]
-  },
-  {
-    featureType: 'transit',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#757575'
-      }
-    ]
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [
-      {
-        color: '#000000'
-      }
-    ]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.fill',
-    stylers: [
-      {
-        color: '#3d3d3d'
-      }
-    ]
+      "featureType": "road",
+      "elementType": "labels",
+      "stylers": [
+          {
+              "visibility": "off"
+          }
+      ]
   }
 ]
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import HomeComponent from './components/HomeComponent';
 import MapComponent from './components/MapComponent';
+import AboutComponent from './components/AboutComponent';
 
-ReactDOM.render(<MapComponent cityid={1} />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Route exact path="/" render={() => <HomeComponent />} />
+        <Route exact path="/map" render={() => <MapComponent cityid={1} />} />
+        <Route exact path="/about" render={() => <AboutComponent />} />
+    </Router>,
+document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
